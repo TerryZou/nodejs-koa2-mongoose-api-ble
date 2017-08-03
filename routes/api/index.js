cbaf29e3284160b80fa1cfee643b22822b2304f3
TerryZou <zouxl_it@163.com>
2017-08-03 14:54:36 +0800

    update root for url format
    update root for url format

--- routes/api/index.js
+++ routes/api/index.js
@@ -8,8 +8,8 @@ var BleMobileInterval_router = require('./BleMobileInterval_router');
 var BleDeviceInfo_router = require('./BleDeviceInfo_router');
 
 router.use('/users', user_router.routes(), user_router.allowedMethods());
-router.use('/bleconnect', BleConnect_router.routes(), BleConnect_router.allowedMethods());
-router.use('/blescan', BleScan_router.routes(), BleScan_router.allowedMethods());
+router.use('/ble_connect', BleConnect_router.routes(), BleConnect_router.allowedMethods());
+router.use('/ble_scan', BleScan_router.routes(), BleScan_router.allowedMethods());
 router.use('/config', BaseConfig_router.routes(), BaseConfig_router.allowedMethods());
 router.use('/config', DistanceInfo_router.routes(), DistanceInfo_router.allowedMethods());
 router.use('/config', BleMobileInterval_router.routes(), BleMobileInterval_router.allowedMethods());
